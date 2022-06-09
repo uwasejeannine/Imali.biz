ee7cceeb6442b03437a9135e3812e940b72510f8
uwasejeannine <jeannineuwasee@gmail.com>
2022-06-09 16:13:59 +0200

    Push changes to heroku

--- imalibiz/settings.py
+++ imalibiz/settings.py
@@ -131,6 +131,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_HOST = os.environ.get("DJANGO_STATIC_HOST", "")
 STATIC_URL = STATIC_HOST + "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 
 
